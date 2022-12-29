3aee1807d3f9c2dc95bfa193456b23744869c3b8
AlexBatura1988 <alex.batura88@gmail.com>
2022-12-29 03:36:05 +0200

    add dependencies

---
+++ src/main/java/com/example/libraryHibernate/LibraryHibernateApplication.java
@@ -0,0 +1,14 @@
+package com.example.libraryHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class LibraryHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/libraryHibernate/LibraryHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
